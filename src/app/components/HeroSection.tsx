8b581b03cc520b4a02c883a17417a48242834e3e
amaan287 <amaanmirza287@gmail.com>
2025-02-07 12:05:27 +0530

    hero section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,16 +1,27 @@
+import Image from "next/image";
 import { ubuntufont } from "../utils/fonts";
 export default function HeroSection() {
   return (
     <div className="overflow-x-hidden">
-      <div className="w-full h-svh flex flex-col  py-4 px-8 justify-center  ">
-        <div
-          className={`text-pretty text-2xl font-semibold ${ubuntufont.className}`}
-        >
-          Hello! I&apos;m Amaan.
+      <div className="w-full h-svh flex flex-col lg:flex-row lg:items-center lg:justify-center py-40 lg:py-8 gap-10 px-8   ">
+        <div className="flex flex-col gap-1">
+          <div
+            className={` text-wrap text-3xl lg:text-4xl flex flex-col lg:flex-row lg:gap-1 font-semibold ${ubuntufont.className}`}
+          >
+            Hello! <span>I&apos;m Amaan.</span>
+          </div>
+          <div className={`${ubuntufont.className}`}>
+            A multidisciplinary developer with a passion for creating engaging,
+            complicated, entertaining user experiences. ✨
+          </div>
         </div>
-        <div>
-          A multidisciplinary developer with a passion for creating engaging,
-          complicated, entertaining user experiences. ✨
+        <div className="mx-auto">
+          <Image
+            src={"/bitmoji.png"}
+            width={250}
+            height={250}
+            alt="Bitmoji of Amaan"
+          />
         </div>
       </div>
     </div>
