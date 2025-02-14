9190cdb0ad4b22c43ab93c0464b7d37ef846f421
amaan287 <amaanmirza287@gmail.com>
2025-02-15 00:31:33 +0530

    adding projects

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,7 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Projects() {
   return (
     <div>
-      <div></div>
+      <div>
+        <Link href={"https://reviewsphere-production.up.railway.app"}>
+          <Image
+            width={1000}
+            height={700}
+            src={"/review-sphere.png"}
+            className="bg-black"
+            alt="Review sphere"
+          />
+        </Link>
+        <Link
+          href={"https://ai-calculator-production.up.railway.app/"}
+          target="_blank"
+        >
+          <div className="flex flex-row md:flex-col">
+            <Image
+              width={1000}
+              height={700}
+              src={"/ai-calc-question.png"}
+              className="bg-black"
+              alt="AI-Powered Calculator"
+            />
+            <Image
+              width={1000}
+              height={700}
+              src={"/ai-calc-result.png"}
+              className="bg-black"
+              alt="AI-Powered Calculator"
+            />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import { useRef, useEffect } from "react";
 import LocomotiveScroll from "locomotive-scroll";
+import Projects from "./components/Projects";
 
 export default function Home() {
   const scrollRef = useRef(null);
@@ -12,6 +13,17 @@ export default function Home() {
       scroll = new LocomotiveScroll({
         el: scrollRef.current,
         smooth: true,
+        multiplier: 0.5,
+        lerp: 0.09,
+        scrollFromAnywhere: false,
+        smartphone: {
+          smooth: true,
+        },
+        tablet: {
+          smooth: true,
+          breakpoint: 1024,
+        },
+        class: "is-revealed",
       });
     }
     return () => {
@@ -19,9 +31,10 @@ export default function Home() {
     };
   }, []);
   return (
-    <div ref={scrollRef} className="overflow-hidden">
+    <div ref={scrollRef} className="overflow-hidden" data-scroll-container>
       <Header />
       <HeroSection />
+      <Projects />
     </div>
   );
 }
